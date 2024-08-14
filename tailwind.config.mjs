e4503492375a8fd62050f836c95fe1fec46a3cb6
DK <hola.hoon@gmail.com>
2024-08-14 14:01:03 -0400

    feat: implement light/dark mode

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,7 +10,40 @@ export default {
             lg: '768px'
          }
       },
-      extend: {}
+      extend: {
+         colors: {
+            border: 'hsl(var(--border))',
+            ring: 'hsl(var(--ring))',
+            background: 'hsl(var(--background))',
+            foreground: 'hsl(var(--foreground))',
+            primary: {
+               DEFAULT: 'hsl(var(--primary))',
+               foreground: 'hsl(var(--primary-foreground))',
+               blue: '#1D4ED8'
+            },
+            secondary: {
+               DEFAULT: 'hsl(var(--secondary))',
+               foreground: 'hsl(var(--secondary-foreground))'
+            },
+            destructive: {
+               DEFAULT: 'hsl(var(--destructive))',
+               foreground: 'hsl(var(--destructive-foreground))'
+            },
+            muted: {
+               DEFAULT: 'hsl(var(--muted))',
+               foreground: 'hsl(var(--muted-foreground))'
+            },
+            accent: {
+               DEFAULT: 'hsl(var(--accent))',
+               foreground: 'hsl(var(--accent-foreground))'
+            }
+         },
+         borderRadius: {
+            lg: 'var(--radius)',
+            md: 'calc(var(--radius) - 2px)',
+            sm: 'calc(var(--radius) - 4px)'
+         }
+      }
    },
    plugins: []
 }
